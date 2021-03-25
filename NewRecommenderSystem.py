36fcab464e4ea6b475faffe4486b5ec1b5387fc0
Timmy Chan <timmychan301197@gmail.com>
2021-03-25 15:53:23 +0100

    Add load dataset

--- NewRecommenderSystem.py
+++ NewRecommenderSystem.py
@@ -4,3 +4,65 @@ df = pd.read_json('dataset/active1000/20170101', lines=True)
 print(df.iloc[0])
 
 #print(df.to_string()) 
+
+
+
+def load_data(path):
+    """
+    Load events from files and convert to dataframe.
+    """
+    print("Loading dataset...")
+    map_lst=[]
+    for f in os.listdir(path):
+        file_name=os.path.join(path,f)
+        if os.path.isfile(file_name):
+            for line in open(file_name):
+                obj = json.loads(line.strip())
+                if not obj is None:
+                    map_lst.append(obj)
+    return pd.DataFrame(map_lst) 
+
+
+def statistics(df):
+    """
+        Basic statistics based on loaded dataframe
+    """
+    total_num = df.shape[0]
+    
+    print("Total number of events(front page incl.): {}".format(total_num))
+    df.sort_values(by=['userId', 'time'], ascending=True, inplace=True)
+    df_ref = df[df['documentId'].notnull()]
+    num_act = df_ref.shape[0]
+    
+    print("Total number of events(without front page): {}".format(num_act))
+    num_docs = df_ref['documentId'].nunique()
+    
+    print("Total number of documents: {}".format(num_docs))
+    print('Sparsity: {:4.3f}%'.format(float(num_act) / float(1000*num_docs) * 100))
+    df_ref.drop_duplicates(subset=['userId', 'documentId'], inplace=True)
+    print("Total number of events(drop duplicates): {}".format(df_ref.shape[0]))
+    print('Sparsity (drop duplicates): {:4.3f}%'.format(float(df_ref.shape[0]) / float(1000*num_docs) * 100))
+    
+    user_df = df_ref.groupby(['userId']).size().reset_index(name='counts')
+    print("Describe by user:")
+    print(user_df.describe())
+
+
+
+if __name__ == '__main__':
+    df=load_data("dataset/active1000")
+    
+    ###### Get Statistics from dataset ############
+    print("Basic statistics of the dataset...")
+    statistics(df)
+    
+    # ###### Recommendations based on Collaborative Filtering (Matrix Factorization) #######
+    # print("Recommendation based on MF...")
+    # collaborative_filtering(df)
+    
+    # ###### Recommendations based on Content-based Method (Cosine Similarity) ############
+    # print("Recommendation based on content-based method...")
+    # content_recommendation(df, k=20)
+    
+    
+    
